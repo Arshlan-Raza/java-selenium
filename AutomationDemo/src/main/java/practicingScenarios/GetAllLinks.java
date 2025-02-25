049cadf63f12e92e4ca68d52c7717fe40ca48335
Arshlan Raza <98450417+Arshlan-Raza@users.noreply.github.com>
2025-02-25 16:42:53 +0530

    getting all the links in the page

---
+++ AutomationDemo/src/main/java/practicingScenarios/GetAllLinks.java
@@ -0,0 +1,26 @@
+package practicingScenarios;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetAllLinks {
+	
+	static {
+		System.setProperty("webdriver.chrome.driver","C:\\SeleniumWebDriver\\ChromeDriver\\chromedriver-win64\\chromedriver.exe");
+	}
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		List<WebElement> allLinks = driver.findElements(By.xpath("//a"));
+		
+		int count  = allLinks.size();
+		System.out.println("Number of Links are:"+count);
+		driver.close();;
+	}
+	
+}
