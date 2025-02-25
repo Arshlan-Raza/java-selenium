150e5fdbcf36a5e90479e39c93563a75348c3170
Arshlan Raza <98450417+Arshlan-Raza@users.noreply.github.com>
2025-02-25 16:42:05 +0530

    learnt drop down menu handling

---
+++ AutomationDemo/src/main/java/practicing/LearningDropDown.java
@@ -0,0 +1,26 @@
+package practicing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearningDropDown {
+    static {
+        System.setProperty("webdriver.chrome.driver", "C:\\SeleniumWebDriver\\ChromeDriver\\chromedriver-win64\\chromedriver.exe");
+    }
+    
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://jqueryui.com/selectmenu/");
+
+        driver.switchTo().frame(0);
+
+        WebElement firstOption = driver.findElement(By.xpath("//span[@id='speed-button']"));
+        firstOption.click();
+
+        driver.findElement(By.xpath("//li[@class='ui-menu-item']//div[text()='Slower']")).click();
+
+        driver.quit();
+    }
+}
