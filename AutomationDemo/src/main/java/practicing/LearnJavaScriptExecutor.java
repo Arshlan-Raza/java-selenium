caf991a1d5713f5ba4fb07f36ef458551cf5555f
Arshlan Raza <98450417+Arshlan-Raza@users.noreply.github.com>
2025-02-25 18:28:22 +0530

    learnt javaexecutor

---
+++ AutomationDemo/src/main/java/practicing/LearnJavaScriptExecutor.java
@@ -0,0 +1,43 @@
+package practicing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnJavaScriptExecutor {
+	
+	static {
+		System.setProperty("webdriver.chrome.driver","C:\\SeleniumWebDriver\\ChromeDriver\\chromedriver-win64\\chromedriver.exe");
+	}
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver() ;
+		
+//		driver.get("https://www.google.com");
+		driver.get("https://demoqa.com/");
+		
+        JavascriptExecutor jsEx = (JavascriptExecutor) driver;
+        
+//        jsEx.executeScript("alert('Hello! This is javascript executor')");
+        
+        WebElement ele = driver.findElement(By.xpath("//h5[text()='Book Store Application']"));
+        
+        jsEx.executeScript("arguments[0].scrollIntoView(true);", ele);
+        
+        jsEx.executeScript("arguments[0].click();", ele);
+        
+        String title = (String) jsEx.executeScript("return document.title;");
+        System.out.println("Page Title: " + title);
+
+//        WebElement element = driver.findElement(By.id("elementId"));
+//        jsExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+
+        
+        
+        
+		
+	}
+
+}
